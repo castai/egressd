3f71d5e2ea83990c6667bd6a701e4a6fb871d873
anjmao <andzej@cast.ai>
2022-12-02 21:48:01 +0200

    Add helm chart

--- examples/vector/setup.sh
+++ examples/vector/setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-kubectl create ns egressd || true
+helm upgrade -i egressd ../../charts/egressd -n egressd --create-namespace
 helm upgrade -i grafana-egressd grafana/grafana -n egressd -f ./grafana-values.yaml
 helm upgrade -i loki-egressd grafana/loki-simple-scalable -n egressd -f ./loki-values.yaml
 helm upgrade -i vector-egressd vector/vector -n egressd -f ./vector-values.yaml
