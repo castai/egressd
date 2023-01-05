de001fc6a52be9c5f65e051a4da66642d3a08517
anjmao <andzej.maciusovic@gmail.com>
2023-01-05 18:03:32 +0200

    Omit empty json fields

--- collector/types.go
+++ collector/types.go
@@ -2,16 +2,16 @@ package collector
 
 type PodNetworkMetric struct {
 	SrcIP        string `json:"src_ip"`
-	SrcPod       string `json:"src_pod"`
-	SrcNamespace string `json:"src_namespace"`
-	SrcNode      string `json:"src_node"`
-	SrcZone      string `json:"src_zone"`
+	SrcPod       string `json:"src_pod,omitempty"`
+	SrcNamespace string `json:"src_namespace,omitempty"`
+	SrcNode      string `json:"src_node,omitempty"`
+	SrcZone      string `json:"src_zone,omitempty"`
 	DstIP        string `json:"dst_ip"`
 	DstIPType    string `json:"dst_ip_type"`
-	DstPod       string `json:"dst_pod"`
-	DstNamespace string `json:"dst_namespace"`
-	DstNode      string `json:"dst_node"`
-	DstZone      string `json:"dst_zone"`
+	DstPod       string `json:"dst_pod,omitempty"`
+	DstNamespace string `json:"dst_namespace,omitempty"`
+	DstNode      string `json:"dst_node,omitempty"`
+	DstZone      string `json:"dst_zone,omitempty"`
 	TxBytes      uint64 `json:"tx_bytes"`
 	TxPackets    uint64 `json:"tx_packets"`
 	RxBytes      uint64 `json:"rx_bytes"`
