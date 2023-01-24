ee503848037c84aefac93fb0b936a4a2f479a48d
Matas Kulkovas <matas.k@cast.ai>
2023-01-24 14:25:18 +0200

    remove flusher, revert exporter

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -2,15 +2,12 @@ package exporter
 
 import (
 	"context"
-	"time"
 
+	"github.com/castai/egressd/collector"
+	"github.com/castai/egressd/metrics"
 	"github.com/cilium/lumberjack/v2"
 	jsoniter "github.com/json-iterator/go"
 	"github.com/sirupsen/logrus"
-
-	"github.com/castai/egressd/collector"
-	"github.com/castai/egressd/flusher"
-	"github.com/castai/egressd/metrics"
 )
 
 type metricsChanGetter interface {
@@ -18,7 +15,6 @@ type metricsChanGetter interface {
 }
 
 type Config struct {
-	FlushInterval       time.Duration
 	ExportFilename      string
 	ExportFileMaxSizeMB int
 	MaxBackups          int
@@ -26,19 +22,10 @@ type Config struct {
 }
 
 func New(cfg Config, log logrus.FieldLogger, metrics metricsChanGetter) *Exporter {
-	writer := &lumberjack.Logger{
-		Filename:   cfg.ExportFilename,
-		MaxSize:    cfg.ExportFileMaxSizeMB,
-		MaxBackups: cfg.MaxBackups,
-		Compress:   cfg.Compress,
-	}
-	dump := flusher.NewConntrackDump(jsoniter.NewEncoder(writer))
-
 	return &Exporter{
 		cfg:     cfg,
 		log:     log,
 		metrics: metrics,
-		dump:    dump,
 	}
 }
 
@@ -46,25 +33,26 @@ type Exporter struct {
 	cfg     Config
 	log     logrus.FieldLogger
 	metrics metricsChanGetter
-
-	dump *flusher.ConntrackDump
 }
 
 func (e *Exporter) Start(ctx context.Context) error {
-	ticker := time.NewTicker(e.cfg.FlushInterval)
+	writer := &lumberjack.Logger{
+		Filename:   e.cfg.ExportFilename,
+		MaxSize:    e.cfg.ExportFileMaxSizeMB,
+		MaxBackups: e.cfg.MaxBackups,
+		Compress:   e.cfg.Compress,
+	}
+
+	encoder := jsoniter.NewEncoder(writer)
+
 	for {
 		select {
 		case <-ctx.Done():
 			return ctx.Err()
 		case metric := <-e.metrics.GetMetricsChan():
 			metrics.IncExportedEvents()
-			e.dump.Dump(metric)
-		case <-ticker.C:
-			cnt, err := e.dump.Flush()
-			if err != nil {
-				e.log.Errorf("flushing metrics: %v", err)
-			} else {
-				e.log.Infof("flushed %d metrics", cnt)
+			if err := encoder.Encode(metric); err != nil {
+				e.log.Errorf("writing metric to logs: %v", err)
 			}
 		}
 	}
