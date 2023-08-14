c6c654126e69666c1efb291693867e67e3e7bde3
Karolis Labrencis <karolis@labrencis.lt>
2023-08-14 15:16:41 +0300

    Build on darwin: move linux-specific tracer code to tracer_linux.go

---
+++ ebpf/tracer_nolinux.go
@@ -0,0 +1,20 @@
+//go:build !linux
+// +build !linux
+
+package ebpf
+
+import (
+	"context"
+)
+
+func (t *Tracer) Run(ctx context.Context) error {
+	panic("not implemented on non-linux")
+}
+
+func (t *Tracer) Events() <-chan DNSEvent {
+	return t.events
+}
+
+func IsKernelBTFAvailable() bool {
+	return false
+}
