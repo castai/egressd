36aeb7d4a569dba828225c77cca20b11b88220e7
Roman Melnyk <roman.melnyk20071992@gmail.com>
2023-10-17 15:43:02 +0300

    add arm support

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ ARG BPFTOOL_IMAGE=ghcr.io/castai/egressd/bpftool@sha256:93f06b391f8e821cef06294b
 FROM ${BPFTOOL_IMAGE} as bpftool-dist
 
 FROM gcr.io/distroless/static-debian11
+ARG TARGETARCH
 COPY --from=bpftool-dist /bin/bpftool /bin/bpftool
-COPY ./bin/egressd /usr/local/bin/egressd
+COPY ./bin/egressd-$TARGETARCH /usr/local/bin/egressd
 ENTRYPOINT ["/usr/local/bin/egressd"]
--- ebpf/generate.go
+++ ebpf/generate.go
@@ -1,3 +1,4 @@
 package ebpf
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang-14 -cflags $BPF_CFLAGS bpf ./c/egressd.c -- -I./c/include
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang-14 -target arm64 -cflags $BPF_CFLAGS bpf ./c/egressd.c -- -I./c/include
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang-14 -target amd64 -cflags $BPF_CFLAGS bpf ./c/egressd.c -- -I./c/include
